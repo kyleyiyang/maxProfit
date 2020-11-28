e7780329791fa661a5b2003d3bec4ac36425748d
kyleyiyang <kyleyiyang@gmail.com>
2020-11-28 17:07:14 -0500

    Update maxProfit.java

--- maxProfit.java
+++ maxProfit.java
@@ -1,13 +1,13 @@
-class Solution {
-    public int maxProfit(int[] prices) {
-        int max=0;
-        for (int i=0;i<prices.length-1;i++) {
-            for (int j=i+1;j<prices.length;j++) {
-                if (prices[j]>=prices[i] && prices[j]-prices[i]>max) {
-                    max=prices[j]-prices[i];
-                }
-            }
+public class Solution {
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
         }
-        return max;
+        return maxprofit;
     }
 }
